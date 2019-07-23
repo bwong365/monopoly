3002ad3e3cc2634f634e38abcd4c0655c1d5843d
Ben Wong <bwong365@outlook.com>
2019-07-22 18:07:59 -0600

    Prelim setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="m-4">Welcome to the Monopoly Bank!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-5 col-sm-4 offset-sm-1 col-lg-3 offset-lg-2 text-center">
+        <RouterLink to="/newgame"><h4>Start a new game</h4></RouterLink>
+      </div>
+      <div class="col-5 offset-2 col-sm-4 offset-sm-2 col-lg-3 text-center">
+        <RouterLink to="/"><h4>Continue an existing game</h4></RouterLink>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
---
+++ src/views/NewGame.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>New Game</h2>
+    <div class="input-group" v-for="(player, index) in players" :key="index">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Name:</span>
+      </div>
+      <input type="text" class="form-control" :placeholder=`Player`>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['players'])
+  }
+}
+</script>
